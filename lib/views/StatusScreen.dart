d92cb99c71d752208f78767a6992509a63c5b974
Pranav Kapoor <pranavkapoorr@gmail.com>
2018-07-09 14:49:42 +0100

    sorted statuses

--- lib/views/StatusScreen.dart
+++ lib/views/StatusScreen.dart
@@ -7,26 +7,41 @@ class StatusScreen extends StatefulWidget {
 
 }
 class _StatusScreenState extends State<StatusScreen>{
+  List<DataModel> statuses = new List();
+
+  @override
+  void initState() {
+    super.initState();
+    dummyDataChat.forEach((e){
+      statuses.add(e);
+    });
+    statuses.sort((a,b)=>a.time.compareTo(b.time));
+  }
   @override
   Widget build(BuildContext context) {
     return new ListView.builder(
-          itemCount: dummyDataChat.length,
+          itemCount: statuses.length,
           itemBuilder: (context,index) => new Column(
             children: <Widget>[
               new Divider(height: 10.0),
               new ListTile(
-                leading: new CircleAvatar(backgroundImage:new NetworkImage(dummyDataChat[index].avatarUrl),backgroundColor: Colors.grey,radius: 25.0,),
+                leading: new CircleAvatar(backgroundImage:new NetworkImage(statuses[index].avatarUrl),backgroundColor: Colors.grey,radius: 25.0,),
                 title: new Row(
                     mainAxisAlignment:MainAxisAlignment.spaceBetween ,
                     children: <Widget>[
-                      new Text(dummyDataChat[index].senderName,style: new TextStyle(fontWeight: FontWeight.bold),)]
+                      new Text(statuses[index].senderName,style: new TextStyle(fontWeight: FontWeight.bold),)]
                 ),
-                subtitle: new Text(dummyDataChat[index].time,style: new TextStyle(color: Colors.grey, fontSize: 14.0)),
+                subtitle: new Text(statuses[index].time,style: new TextStyle(color: Colors.grey, fontSize: 14.0)),
               )
 
             ],
           )
     );
+  }
+@override
+  void dispose() {
+    statuses.clear();
+    super.dispose();
   }
 
 }
